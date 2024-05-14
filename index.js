8e6c5f4e40a4aaf59dfdeecdff4fdb94d8a99b51
janghw33 <janghw33@gmail.com>
2024-05-14 11:11:47 +0200

    add & done

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // Temp button functions
-addTodo = () => {};
+
+const AddTodo = () => {
+    var inputText = document.getElementById('todo-input').value; 
+    var todoList = document.getElementById('todo-list'); 
+
+    var newListItem = document.createElement('li');
+    var checkBox = document.createElement('input'); 
+    checkBox.type = "checkbox"; 
+    var spanText = document.createElement('span'); 
+    spanText.textContent = inputText; 
+
+    checkBox.addEventListener('change', function() {
+        if (checkBox.checked) {
+            spanText.style.textDecoration = "line-through"; 
+        } else {
+            spanText.style.textDecoration = "none"; 
+        }
+    });
+
+    newListItem.appendChild(spanText);
+    newListItem.appendChild(checkBox);
+    todoList.appendChild(newListItem);
+
+    document.getElementById('todo-input').value = '';
+}
 
 deleteTodo = () => {};
 
